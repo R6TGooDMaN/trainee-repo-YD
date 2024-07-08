9ad790faeed5d98ca75dc98349c3b317ce074d7e
R6TGooDMaN <dimassik20225@gmail.com>
2024-07-08 14:27:45 +0300

    Product clas changes

--- product-service/src/main/java/org/trainee/productservice/model/Product.java
+++ product-service/src/main/java/org/trainee/productservice/model/Product.java
@@ -1,5 +1,6 @@
 package org.trainee.productservice.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,5 +29,8 @@ public class Product {
     private Long id;
     private String name;
     private String description;
+
+    @Column(columnDefinition = "INTEGER CHECK (price > 0)")
     private Integer price;
+
 }
